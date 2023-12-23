88104859c4f5d93ee4453e6f60ef17d20f786ce9
Luis <150976352+lmcs73@users.noreply.github.com>
2023-12-23 01:38:22 +0000

    Estilizacao e mover cartoes

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-// Aqui, você pode adicionar a lógica de interação do Kanban
+function addTask() {
+    var taskName = document.getElementById('taskName').value;
+    if (!taskName) {
+        alert("Por favor, insira um nome para a tarefa.");
+        return;
+    }
+
+    var taskCard = document.createElement('div');
+    taskCard.classList.add('task-card');
+    taskCard.innerText = taskName;
+
+    var todoColumn = document.querySelector('.kanban-column.todo');
+    todoColumn.appendChild(taskCard);
+
+    document.getElementById('taskName').value = ''; // Limpar o campo após adicionar
+}
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Lógica inicializada após o carregamento do documento 
-}); 
+    // Lógica inicializada após o carregamento do documento
+});
