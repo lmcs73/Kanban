2c36effeeee3f1a264153ca81e09516ff452c6fa
Luis <150976352+lmcs73@users.noreply.github.com>
2023-12-23 02:47:03 +0000

    Pagina inicial

--- script.js
+++ script.js
@@ -1,20 +1,5 @@
-function addTask() {
-    var taskName = document.getElementById('taskName').value;
-    if (!taskName) {
-        alert("Por favor, insira um nome para a tarefa.");
-        return;
-    }
-
-    var taskCard = document.createElement('div');
-    taskCard.classList.add('task-card');
-    taskCard.innerText = taskName;
-
-    var todoColumn = document.querySelector('.kanban-column.todo');
-    todoColumn.appendChild(taskCard);
-
-    document.getElementById('taskName').value = ''; // Limpar o campo após adicionar
-}
+// Lógica de navegação
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Lógica inicializada após o carregamento do documento
+    // Lógica após carregamento do documento
 });
