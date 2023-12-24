523ba9428b25351beb0db46474b83c3adadf72c9
Luis <150976352+lmcs73@users.noreply.github.com>
2023-12-24 23:22:07 +0000

    logica

--- script.js
+++ script.js
@@ -3,3 +3,16 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Lógica após carregamento do documento
 });
+function addColumn() {
+    // Implementar a lógica para adicionar uma nova coluna
+}
+
+function renameColumn(columnId) {
+    // Implementar a lógica para renomear uma coluna
+}
+
+function deleteColumn(columnId) {
+    // Implementar a lógica para deletar uma coluna
+}
+
+// Adicionar lógica para permitir reordenar colunas (arrastar e soltar)
